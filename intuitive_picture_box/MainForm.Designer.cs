ef19f09b105fb885f98c0104d3af5a3dd692448a
Tom Gregor <tgregor@ivsoftware.com>
2022-07-15 12:21:34 -0600

    Add project files.

---
+++ intuitive_picture_box/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace intuitive_buttons
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.customButton0 = new intuitive_buttons.CustomButton();
+            this.customButton3 = new intuitive_buttons.CustomButton();
+            this.customButton1 = new intuitive_buttons.CustomButton();
+            this.customButton2 = new intuitive_buttons.CustomButton();
+            this.customButton4 = new intuitive_buttons.CustomButton();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.customButton0, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.customButton1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.customButton2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.customButton3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.customButton4, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(22, 40);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(430, 86);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // customButton0
+            // 
+            this.customButton0.Location = new System.Drawing.Point(89, 3);
+            this.customButton0.Name = "customButton4";
+            this.customButton0.Size = new System.Drawing.Size(80, 80);
+            this.customButton0.TabIndex = 2;
+            this.customButton0.Text = "";
+            this.customButton0.UseCompatibleTextRendering = true;
+            this.customButton0.UseVisualStyleBackColor = true;
+            // 
+            // customButton1
+            // 
+            this.customButton1.Location = new System.Drawing.Point(175, 3);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(80, 80);
+            this.customButton1.TabIndex = 0;
+            this.customButton1.Text = "";
+            this.customButton1.UseCompatibleTextRendering = true;
+            this.customButton1.UseVisualStyleBackColor = true;
+            // 
+            // customButton2
+            // 
+            this.customButton2.Location = new System.Drawing.Point(261, 3);
+            this.customButton2.Name = "customButton2";
+            this.customButton2.Size = new System.Drawing.Size(80, 80);
+            this.customButton2.TabIndex = 0;
+            this.customButton2.Text = "";
+            this.customButton2.UseCompatibleTextRendering = true;
+            this.customButton2.UseVisualStyleBackColor = true;
+            // 
+            // customButton3
+            // 
+            this.customButton3.Location = new System.Drawing.Point(347, 3);
+            this.customButton3.Name = "customButton3";
+            this.customButton3.Size = new System.Drawing.Size(80, 80);
+            this.customButton3.TabIndex = 1;
+            this.customButton3.Text = "";
+            this.customButton3.UseCompatibleTextRendering = true;
+            this.customButton3.UseVisualStyleBackColor = true;
+            // 
+            // customButton4
+            // 
+            this.customButton4.Location = new System.Drawing.Point(3, 3);
+            this.customButton4.Name = "button1";
+            this.customButton4.Size = new System.Drawing.Size(80, 80);
+            this.customButton4.TabIndex = 0;
+            this.customButton4.Text = "";
+            this.customButton4.UseCompatibleTextRendering = true;
+            this.customButton4.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 279);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private intuitive_buttons.CustomButton customButton0;
+        private intuitive_buttons.CustomButton customButton1;
+        private intuitive_buttons.CustomButton customButton2;
+        private intuitive_buttons.CustomButton customButton3;
+        private intuitive_buttons.CustomButton customButton4;
+    }
+}
+
---
+++ intuitive_picture_box/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace intuitive_buttons
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            customButton0.Text = "\uE800";
+            customButton1.Text = "\uE801";
+            customButton2.Text = "\uE802";
+            customButton3.Text = "\uE803";
+            customButton4.Text = "\uE804";
+        }
+    }
+    class CustomButton : Button
+    {
+        public CustomButton()
+        {
+            UseCompatibleTextRendering = true;
+            TextAlign = ContentAlignment.MiddleCenter;
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            if (!DesignMode)
+            {
+                initFont();
+            }
+        }
+
+        private void initFont()
+        {
+            if(_privateFontCollection == null)
+            {
+                _privateFontCollection = new PrivateFontCollection();
+                var path = Path.Combine(Path.GetDirectoryName(
+                    Assembly.GetEntryAssembly().Location),
+                    "Fonts",
+                    "flashlight-filter-history-favorite-search.ttf");
+                _privateFontCollection.AddFontFile(path); 
+                var fontFamily = _privateFontCollection.Families[0];
+                GlyphFont = new Font(fontFamily, 16F);
+            }
+            Font = GlyphFont;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            BackgroundImage = Resources.buttonDown;
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            BackgroundImage = null;
+        }
+
+        private static PrivateFontCollection _privateFontCollection = null;
+
+        public static Font GlyphFont { get; private set; }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _privateFontCollection?.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
